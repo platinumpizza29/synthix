097c34e5881e9ed93af7dd38ed08c3f97ffebd73
platinumpizza29 <kbelgi1@gmail.com>
2024-05-20 13:57:33 +0100

    UI Update
    Home page UI updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,15 @@ export default function HomePage() {
   return (
     <main className="h-screen w-screen">
       <NavbarComp />
-      <div className="text-3xl md:text-7xl flex flex-col justify-center items-center py-8">
-        <h1>Generate anything</h1>
+      <div className="text-3xl md:text-7xl flex flex-col justify-center items-center py-8 space-y-4">
+        <h1>Generate <span className="bg-primary p-2 rounded-2xl text-white">Anything</span></h1>
         <h1>what's in your mind</h1>
-        <button className="btn btn-primary rounded-2xl">Get Started</button>
+        <button className="btn btn-primary rounded-2xl my-8">Get Started</button>
+        <div className="p-2 md:p-12 w-full">
+          <div className="mockup-window border bg-base-300">
+            <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+          </div>
+        </div>
       </div>
     </main>
   );
