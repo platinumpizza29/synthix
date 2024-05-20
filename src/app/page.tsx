bfe082c0bcb52b12ea20f8e1fea123045c2bffc0
platinumpizza29 <kbelgi1@gmail.com>
2024-05-20 15:29:11 +0100

    UI Update - Landing Page
    Blob animation and background effects added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,15 @@ export default function HomePage() {
     <main className="h-screen w-screen">
       <NavbarComp />
       <div className="text-3xl md:text-7xl flex flex-col justify-center items-center py-8 space-y-4">
-        <h1>Generate <span className="bg-primary p-2 rounded-2xl text-white">Anything</span></h1>
-        <h1>what&apos;s in your mind</h1>
-        <button className="btn btn-primary rounded-2xl my-8">Get Started</button>
-        <div className="p-2 md:p-12 w-full">
-          <div className="mockup-window border bg-base-300">
-            <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
-          </div>
+        <div className="relative p-20">
+          <div className="absolute top-10 -left-4 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
+          <div className="absolute top-10 -right-4 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
+          <div className="absolute -bottom-20 left-20 w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70i animate-blob"></div>
+        </div>
+        <div className="z-1">
+          <h1>Generate <span className="bg-primary p-2 rounded-2xl text-white">Anything</span></h1>
+          <h1>what&apos;s in your mind</h1>
+          <button className="btn btn-primary rounded-2xl my-8">Get Started</button>
         </div>
       </div>
     </main>
